e9d8e6b698069259d10c6e43428392407592a1dd
K4ge <779938689@qq.com>
2019-12-13 18:26:51 +0800

    commit test.

---
+++ src/serialize/Test.java
@@ -0,0 +1,8 @@
+package serialize;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("calc");
+    }
+}
